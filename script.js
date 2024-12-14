33e692559c385adad9dc1f7257958a486b63a3ee
Sughosh <sughoshathreya1@gmail.com>
2024-12-14 15:47:55 +0530

    removed token

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('fetchBtn').addEventListener('click',async()=>{
         userDetails.innerHTML='<p>Please enter a username.</p>';
         return
     }
-    const token="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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"
+    // const token="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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"
     const apiUrl=`https://api.github.com/users/${username}`;
 
     await fetch(apiUrl)
